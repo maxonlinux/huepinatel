6dd2341226202332cd4755c8ede462ccc5088702
maxonlinux <30315332+maxonlinux@users.noreply.github.com>
2022-07-23 02:10:21 +0300

    initial

---
+++ script.js
@@ -0,0 +1,29 @@
+const check = document.querySelector(".check");
+const doc = document.querySelector(".doc");
+const btn = document.querySelector('.button-common')
+const norobot = document.querySelector('.not-robot')
+
+const loader = document.querySelector(".loader");
+
+check.addEventListener("click", function () {
+    setTimeout(() => {
+        check.checked = false
+        norobot.classList.add('inactive')
+        btn.classList.remove('inactive')
+
+        btn.addEventListener("click", function () {
+            if (!doc.classList.contains('inactive')) {
+                doc.classList.add('inactive')
+            }
+            loader.classList.remove("inactive");
+            loader.style.setProperty("--width", loader.clientWidth + "px");
+            setTimeout(() => {
+                loader.classList.add("inactive");
+                doc.classList.remove("inactive");
+
+                norobot.classList.remove('inactive')
+                btn.classList.add('inactive')
+            }, 2500);
+        });
+    }, 1000);
+});
