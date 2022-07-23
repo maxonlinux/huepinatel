9841af48abadb076e0578b01f6521d592d394992
maxonlinux <30315332+maxonlinux@users.noreply.github.com>
2022-07-23 03:11:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 const check = document.querySelector(".check");
 const doc = document.querySelector(".doc");
-const btn = document.querySelector('.button-common')
-const norobot = document.querySelector('.not-robot')
+const btn = document.querySelector(".button-common");
+const norobot = document.querySelector(".not-robot");
 
 const loader = document.querySelector(".loader");
 
 check.addEventListener("click", function () {
     setTimeout(() => {
-        check.checked = false
-        norobot.classList.add('inactive')
-        btn.classList.remove('inactive')
+        check.checked = false;
+        norobot.classList.add("inactive");
+        btn.classList.remove("inactive");
 
         btn.addEventListener("click", function () {
-            if (!doc.classList.contains('inactive')) {
-                doc.classList.add('inactive')
-            }
-            loader.classList.remove("inactive");
-            loader.style.setProperty("--width", loader.clientWidth + "px");
-            setTimeout(() => {
-                loader.classList.add("inactive");
-                doc.classList.remove("inactive");
+                if (!doc.classList.contains("inactive")) {
+                    doc.classList.add("inactive");
+                }
+                loader.classList.remove("inactive");
+                setTimeout(() => {
+                    loader.style.setProperty(
+                        "--width",
+                        loader.clientWidth + "px"
+                    );
+                    setTimeout(() => {
+                        loader.classList.add("inactive");
+                        doc.classList.remove("inactive");
 
-                norobot.classList.remove('inactive')
-                btn.classList.add('inactive')
-            }, 3000);
-        });
+                        norobot.classList.remove("inactive");
+                        btn.classList.add("inactive");
+                    }, 3000);
+                });
+            },0);
     }, 1000);
 });
